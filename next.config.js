b5595e693ce4ea3575a7b1dadfefcdb01fa6d7e5
Link <61148194+Linkbreathe@users.noreply.github.com>
2024-08-03 18:49:09 +0800

    some changes in eslint

--- next.config.js
+++ next.config.js
@@ -2,14 +2,16 @@
 // settings such as picture
 const nextConfig = {
     images:{
-        timeout: 3000, // 默认是1000，这里设置为3000毫秒
         domains:["avatars.githubusercontent.com","firebasestorage.googleapis.com","tenor.com"],
         unoptimized: true,
     },
     env: {
         GOOGLE_MAP_API_KEY: process.env.GOOGLE_MAP_API_KEY,
     },
-    reactStrictMode: false
+    reactStrictMode: false,
+    eslint: {
+        ignoreDuringBuilds: true,
+    },
 }
 
 module.exports = nextConfig
--- src/app/contact/page.jsx
+++ src/app/contact/page.jsx
@@ -3,7 +3,7 @@ import { PersonInfo } from '@/components/personInfo/PersonInfo';
 
 const ContactPage = () => {
     return (
-        <div className='min-h-max'>
+        <div className='min-h-max h-screen'>
             <PersonInfo />
         </div>
     )
