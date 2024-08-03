899e1fd44bdbfbe0d68b10387d92d32d69a1407f
Link <61148194+Linkbreathe@users.noreply.github.com>
2024-08-03 23:10:08 +0800

    web api prefix change

--- src/app/noteMap/page.jsx
+++ src/app/noteMap/page.jsx
@@ -2,21 +2,22 @@
 import React from 'react'
 import WEB_API from '@/utils/prefix'
 import NoteMap from '@/components/noteMap/NoteMap';
-// const getData = async () => {
-//     const res = await fetch(`${WEB_API}/geoLocate`)
-//     if (!res.ok) {
-//         // throw new Error("Failed")
-//         return { posts: [], count: 0 };
-//     } else {
-//         return res.json()
-//     }
-// }
+const getData = async () => {
+    const res = await fetch(`${WEB_API}/geoLocate`)
+    if (!res.ok) {
+        // throw new Error("Failed")
+        return { posts: [], count: 0 };
+    } else {
+        return res.json()
+    }
+}
 
 const page = async () => {
-    // const { posts, count } = await getData();
+    const { posts, count } = await getData();
+    console.log(posts)
     return (
         <div>
-            {/* <NoteMap posts={posts} /> */}
+            <NoteMap posts={posts} />
         </div>
 
     )
