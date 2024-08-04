799f7627e438fd390179eb9593536620ed3218da
Link <61148194+Linkbreathe@users.noreply.github.com>
2024-08-04 18:38:28 +0800

    prefix issues and modify loading page v2

--- src/utils/prefix.js
+++ src/utils/prefix.js
@@ -1,4 +1,4 @@
 // const WEB_API = "http://localhost:3000/api"
-// const WEB_API = "https://next-blog-git-verceltest-links-projects-4b53c554.vercel.app/api";
-const WEB_API = "https://next-blog-links-projects-4b53c554.vercel.app/api";
+const WEB_API = "https://next-blog-git-verceltest-links-projects-4b53c554.vercel.app/api";
+// const WEB_API = "https://next-blog-links-projects-4b53c554.vercel.app/api";
 export default WEB_API;
